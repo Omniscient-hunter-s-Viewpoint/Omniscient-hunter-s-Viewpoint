c7a87518a73211d039e1d188e74d3083de21c705
hyowon <109724865+wonhwo@users.noreply.github.com>
2024-08-21 17:45:05 +0900

    기초 커밋

---
+++ ohv_pront/src/component/main/js/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../scss/main.scss';
+const Main = () => {
+    return (
+        <div>
+            <h1 className="test">이게 메인이야 알았어!?</h1>
+        </div>
+    );
+};
+
+export default Main;
