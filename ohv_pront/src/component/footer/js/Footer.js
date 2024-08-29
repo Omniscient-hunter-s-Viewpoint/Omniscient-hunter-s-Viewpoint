4597a28c4e3233b787310f958cc82d643bdc5cdc
H4YI3R <whguswns1977@gmail.com>
2024-08-29 13:37:36 +0900

    임시저장

---
+++ ohv_pront/src/component/footer/js/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>이효원 바보</h1>
+        </div>
+    );
+};
+
+export default Footer;
--- ohv_pront/src/component/main/js/main.js
+++ ohv_pront/src/component/main/js/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../scss/main.scss';
+import Footer from '../../footer/js/Footer';
 const Main = () => {
     return (
         <div>
-            <h1 className="test">이게 메인이야 알았어!?</h1>
         </div>
     );
 };
